038b76fa13f0d67dfbb86e9ef7891e5dc673d9eb
Reander Agulto <reanderagulto29@gmail.com>
2023-02-08 00:04:55 +0800

    Day 01

---
+++ 01_Day_Introduction/datatypes.js
@@ -0,0 +1,5 @@
+console.log(typeof "Reander");
+console.log(typeof 29);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ 01_Day_Introduction/exercise1.js
@@ -0,0 +1,5 @@
+// Comments can make code readable
+// Welcome to 30DaysOfJavascript
+/* 
+    Comments can make code readable, easy to reuse and informative.
+ */
