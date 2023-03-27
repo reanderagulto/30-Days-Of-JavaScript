1a569ba0a285c4643048571d2a77f22ccb3e24fe
Reander Agulto <reanderagulto29@gmail.com>
2023-03-27 22:37:04 +0800

    Updated Day 25 - Finished Activity

--- 25_Day_World_countries_data_visualization_1/25_day_starter/js/main.js
+++ 25_Day_World_countries_data_visualization_1/25_day_starter/js/main.js
@@ -18,10 +18,12 @@ for (const occ in occurrences){
         no: occurrences[occ]
     })
 }
-
 langData.sort((a, b) => {
     return b.no - a.no
 })
+
+const maxPop = 2000000000
+const maxLang = 100
 
 const population_btn = document.querySelector('.population')
 const languages_btn = document.querySelector('.languages')
@@ -32,17 +34,62 @@ const stat = document.querySelector('#stat')
 
 headerSub.textContent = `Currently We have ${countries_data.length} countries`
 
-
 population_btn.addEventListener('click', e => {
+    stat.innerHTML = ''
+    data = document.createElement('table')
     for (let index = 0; index < 10; index++) {
-        data = document.createElement('div')
-        html = `<span class="name">${population[index].name}</span>
-                <span class="no">${population[index].population.toLocaleString("en-US")}</span>`
-        data.innerHTML = html
-        stat.appendChild(data)
+        tr = document.createElement('tr')
+            tdName = document.createElement('td')
+                spanName = document.createElement('span')
+                spanName.classList.add('name')
+                spanName.textContent = population[index].name
+            tdName.appendChild(spanName)
+            tdBar = document.createElement('td')
+            tdBar.classList.add('progress-bar')
+                divBar = document.createElement('div')
+                divBar.classList.add('progress')
+                divBar.style.width = `${(population[index].population / maxPop) * 100}%`
+            tdBar.appendChild(divBar)
+            tdNo = document.createElement('td')
+                spanNo = document.createElement('span')
+                spanNo.classList.add('no')
+                spanNo.textContent = population[index].population.toLocaleString("en-US")
+                spanNo.style.textAlign = 'left'
+            tdNo.appendChild(spanNo)
+        tr.appendChild(tdName)
+        tr.appendChild(tdBar)
+        tr.appendChild(tdNo)
+        data.appendChild(tr)
     }
+    stat.appendChild(data)
 })
 
 languages_btn.addEventListener('click', e => {
-
+    stat.innerHTML = ''
+    data = document.createElement('table')
+    for (let index = 0; index < 10; index++) {
+        tr = document.createElement('tr')
+            tdName = document.createElement('td')
+                spanName = document.createElement('span')
+                spanName.classList.add('name')
+                spanName.textContent = langData[index].name
+            tdName.appendChild(spanName)
+            tdBar = document.createElement('td')
+            tdBar.classList.add('progress-bar')
+                divBar = document.createElement('div')
+                divBar.classList.add('progress')
+                divBar.style.width = `${(langData[index].no / maxLang) * 100}%`
+            tdBar.appendChild(divBar)
+            tdNo = document.createElement('td')
+                spanNo = document.createElement('span')
+                spanNo.classList.add('no')
+                spanNo.textContent = langData[index].no.toLocaleString("en-US")
+                spanNo.style.textAlign = 'left'
+            tdNo.appendChild(spanNo)
+        tr.appendChild(tdName)
+        tr.appendChild(tdBar)
+        tr.appendChild(tdNo)
+        data.appendChild(tr)
+    }
+    stat.appendChild(data)
 })
