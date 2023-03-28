15bed7b847dfee3f31c316a6948a151be563255b
Reander Agulto <reanderagulto29@gmail.com>
2023-03-28 20:10:19 +0800

    Day 26 - Updates

--- 26_Day_World_countries_data_visualization_2/26_day_starter/scripts/main.js
+++ 26_Day_World_countries_data_visualization_2/26_day_starter/scripts/main.js
@@ -1,2 +1,27 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const totalCountries = document.querySelector('.total-countries')
+const letterContains = document.querySelector('.letter-contains')
+const startWord = document.querySelector('#starting-word')
+const anyWord = document.querySelector('#any-word')
+const searchBox = document.querySelector('#search-box')
+const resultWrapper = document.querySelector('.results-wrapper')
+let searchBy = ''
+
+const returnResult = (keyword, searchBy) => {
+    return countries.find(element => element.match(`/${searchBy}${keyword}/g`));
+}
+
+startWord.addEventListener('click', e => {    
+    anyWord.classList.remove('clicked')
+    startWord.classList.add('clicked')
+    searchBy = '^'
+})
+anyWord.addEventListener('click', e => {
+    anyWord.classList.add('clicked')
+    startWord.classList.remove('clicked')
+    searchBy = ''
+})
+
+searchBox.addEventListener('keypress', e => {
+    data = returnResult(e.key, searchBy)
+    console.log(data);
+})
