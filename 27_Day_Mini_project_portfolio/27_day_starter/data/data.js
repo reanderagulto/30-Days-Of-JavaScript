cc10e6bca3997cb09c67eb1519972eb40e5e264b
Reander Agulto <reanderagulto29@gmail.com>
2023-03-30 08:30:34 +0800

    Updted Day 27 Activity

--- 27_Day_Mini_project_portfolio/27_day_starter/data/data.js
+++ 27_Day_Mini_project_portfolio/27_day_starter/data/data.js
@@ -1,13 +1,29 @@
 const personal_info = {
-	name: 'Asabeneh Yetayeh',
-	description: 'I am Asabeneh Yetayeh. I am instructor, develop, motivator and content creator. I love educating people and creating educational materials. In January 2019, I also created a teaching material called <strong>JavaScript For Everyone<strong/>. In December 2019, I created <strong>30Days Of Python</strong> challenge and in January 2020 <strong>30 Days Of JavaScript</strong> challenge.',
 	professions: [
-		'Instructor',
-		'Motivational Speaker',
-		'Programmer',
-		'Motivator',
-		'Content Creator',
-		'Educator',
+		{
+			name: 'Instructor',
+			emoji: "🔥",
+		}, 
+		{
+			name: 'Motivational Speaker',
+			emoji: "",
+		}, 
+		{
+			name: 'Programmer',
+			emoji: "",
+		},
+		{
+			name: 'Motivator',
+			emoji: "",
+		},		
+		{
+			name:'Content Creator',
+			emoji:"",
+		},
+		{
+			name: 'Educator',
+			emoji:""
+		}
 	],
 	languages: [
 		'React',
